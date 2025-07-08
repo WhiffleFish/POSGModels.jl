5d10002d510ea14974d49ed2fdb25cb9b1889044
WhiffleFish <tylertronics@gmail.com>
2025-07-08 16:40:07 -0400

    dirty simple game impl

--- src/simple_game.jl
+++ src/simple_game.jl
@@ -6,48 +6,100 @@ using POMDPTools
 
 export SimpleMG
 
-struct SimpleMG <: MG{Int, Tuple{Int, Int}}
-    rewards::Vector{Matrix{Float64}}
-    discount::Float64
-    function SimpleMG(na1, na2; rng=Random.default_rng(), discount=0.95)
-        n_mat = na1 * na2 + 1
-        new([randn(rng, na1, na2) for i ∈ 1:n_mat], discount)
-    end
-    function SimpleMG(v::Vector{Matrix{Float64}}; discount=0.95)
-        new(v, discount)
+const SimpleState = Vector{Tuple{Int, Int}}
+
+push(v::Array, x)= push!(copy(v), x)
+
+next_state(s, a1, a2) = push(s, (a1, a2))
+
+function traverse!(rewards::Dict, s::Vector, d::Int, na::Tuple, max_depth::Int)
+    na1, na2 = na
+    rewards[s] = randn(na...)
+    if d ≥ max_depth
+        return nothing
+    else
+        for a1 ∈ 1:na1, a2 ∈ 1:na2
+            sp = next_state(s, a1, a2)
+            traverse!(rewards, sp, d+1, na, max_depth)
+        end
     end
 end
 
-MarkovGames.initialstate(::SimpleMG) = Deterministic(1)
+#=
+D = 2
+na1 = 2, na2 = 2
+(0,0) => [0,0, 0,0, 0,0]
+(1,1) => [1,1, 0,0, 0,0]
+[(1,1), (2,1)] => [1,1, 2,1, 0,0]
+=#
+struct SimpleMG <: MG{SimpleState, Tuple{Int, Int}}
+    rewards::Dict{Vector{Tuple{Int, Int}}, Matrix{Float64}}
+    max_depth::Int
+    discount::Float64
+    function SimpleMG(na1, na2; discount=0.95, max_depth=2)
+        # depth 0
+        rewards = Dict{SimpleState, Matrix{Float64}}()
+        traverse!(rewards, Tuple{Int,Int}[], 0, (na1, na2), max_depth)
+        new(rewards, max_depth, discount)
+    end
+end
 
-MarkovGames.reward(p::SimpleMG, s, a::Tuple) = p.rewards[s][a...]
+MarkovGames.initialstate(::SimpleMG) = Deterministic(Tuple{Int,Int}[])
 
-MarkovGames.actions(p::SimpleMG) = axes(first(p.rewards))
+MarkovGames.reward(p::SimpleMG, s, a::Tuple) = isterminal(p, s) ? 0.0 : p.rewards[s][a...]
 
-MarkovGames.states(p::SimpleMG) = 0:lastindex(p.rewards)
+MarkovGames.actions(p::SimpleMG) = axes(first(values(p.rewards)))
+
+MarkovGames.states(p::SimpleMG) = push!(collect(keys(p.rewards)), [(-1,-1)])
 
 MarkovGames.discount(p::SimpleMG) = p.discount
 
 function MarkovGames.transition(p::SimpleMG, s, a)
-    return if isone(s)
-        LinearIndices(first(p.rewards))[CartesianIndex(a)] + 1
+    return if isterminal(p, s) # already terminal
+        [(-1,-1)]
+    elseif length(s) == p.max_depth # to terminal
+        [(-1,-1)]
     else
-        0
+        next_state(s, a...)
     end |> Deterministic
 end
 
-MarkovGames.isterminal(::SimpleMG, s) = iszero(s)
+MarkovGames.isterminal(::SimpleMG, s) = !isempty(s) && first(first(s)) === -1
 
-MarkovGames.stateindex(::SimpleMG, s) = s + 1
+function MarkovGames.convert_s(::Type{Vector{T}}, s::SimpleState, p::SimpleMG) where T
+    na1, na2 = length.(actions(p))
+    l = p.max_depth*(na1+na2)
+    if isterminal(p, s)
+        return ones(T, l) .* -1
+    else
+        A = actions(p)
+        na1, na2 = length.(A)
+        v = zeros(T, l)
+        step = na1 + na2
+        for i ∈ eachindex(s)
+            a = s[i]
+            p1_idx = step * (i-1)
+            a1_idx = p1_idx + a[1]
+            v[a1_idx] = one(T)
+            
+            p2_idx = p1_idx + na1
+            a2_idx = p2_idx + a[2]
+            v[a2_idx] = one(T)
+        end
+        return v
+    end
 
-function MarkovGames.convert_s(::Type{Vector{T}}, s::Int, p::SimpleMG) where T
-    v = zeros(T, length(states(p)))
-    v[s + 1] = one(T)
-    return v
+    # return mapreduce(vcat, s) do a 
+    #     v1 = zeros(T, na1)
+    #     v1[a[1]] = one(T)
+    #     v2 = zeros(T, na2)
+    #     v2[a[2]] = one(T)
+    #     vcat(v1,v2) # vcat inside vcat inside vcat inside vcat inside vcat inside vcat
+    # end
 end
 
-function MarkovGames.convert_s(::Type{Int}, v::AbstractVector, p::SimpleMG)
-    return findfirst(isone, v) - 1
-end
+# function MarkovGames.convert_s(::Type{Int}, v::AbstractVector, p::SimpleMG)
+#     return findfirst(isone, v) - 1
+# end
 
 end
