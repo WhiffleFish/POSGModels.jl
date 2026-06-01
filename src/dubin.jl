aff5e94822e1e882ca2ead6d8fe4664ae0991134
WhiffleFish <tylertronics@gmail.com>
2026-05-31 22:33:29 -0600

    type stable dubin transition

--- src/dubin.jl
+++ src/dubin.jl
@@ -42,10 +42,10 @@ end
 
 function rk4step(f, x, h)
     k1 = f(x)
-    k2 = f(@.(x + h * k1 / 2))
-    k3 = f(@.(x + h * k2 / 2))
-    k4 = f(@.(x + h * k3))
-    return @. x + h / 6 * (k1 + 2 * k2 + 2k3 + k4)
+    k2 = f(x + (h / 2) * k1)
+    k3 = f(x + (h / 2) * k2)
+    k4 = f(x + h * k3)
+    return x + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
 end
 
 function dubin_dx(x, V, θ̇)
